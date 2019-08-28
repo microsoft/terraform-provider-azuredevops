dd019d5f5e0d0c5f9973b806339d75287fd04ef3
Nicholas M. Iodice <niiodice@microsoft.com>
2019-08-28 11:06:24 -0500

    Adding aggregated client struct to provider.go. Ran go fmt (#15)
    * Adding aggregated client struct to provider.go. Ran go fmt
    
    * removing binary file

--- terraform-provider/provider-src/src/go.mod
+++ terraform-provider/provider-src/src/go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/hashicorp/terraform v0.12.5
+	github.com/microsoft/azure-devops-go-api/azuredevops v0.0.0-20190822161802-a196ec51000e
 	github.com/stretchr/testify v1.3.0
 )
--- terraform-provider/provider-src/src/provider.go
+++ terraform-provider/provider-src/src/provider.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/build"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/core"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/operations"
 )
+
+// Aggregates all of the underlying clients into a single data
+// type. Each client is ready to use and fully configured with the correct
+// AzDO PAT/organization
+type aggregatedClient struct {
+	CoreClient       *core.Client
+	BuildClient      *build.Client
+	OperationsClient *operations.Client
+}
 
 // Provider - The top level Azure DevOps Provider definition.
 func Provider() *schema.Provider {
 
 	return &schema.Provider{
-		Schema: map[string]*schema.Schema{			
+		Schema: map[string]*schema.Schema{
 			"org_service_url": {
 				Type:        schema.TypeString,
-				Required: 	 true,	
+				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("AZDO_ORG_SERVICE_URL", nil),
 				Description: "The url of the Azure DevOps instance which should be used.",
 			},
 			"personal_access_token": {
 				Type:        schema.TypeString,
-				Required: 	 true,	
+				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("AZDO_PERSONAL_ACCESS_TOKEN", nil),
 				Description: "The personal access token which should be used.",
 			},
