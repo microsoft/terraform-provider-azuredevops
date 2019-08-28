accf79edc679016a66f22f73343c203e9d204b37
David Shaevel <dshaevel@users.noreply.github.com>
2019-08-28 11:55:43 -0500

    adding ConfigureFunc to the Provider object (#17)
    * starting point for day 2 of hack
    
    * adding ConfigureFunc to the Provider object

--- terraform-provider/provider-src/src/provider.go
+++ terraform-provider/provider-src/src/provider.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/microsoft/azure-devops-go-api/azuredevops"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/build"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/core"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/operations"
@@ -14,12 +19,16 @@ type aggregatedClient struct {
 	CoreClient       *core.Client
 	BuildClient      *build.Client
 	OperationsClient *operations.Client
+	ctx              context.Context
 }
 
 // Provider - The top level Azure DevOps Provider definition.
 func Provider() *schema.Provider {
-
-	return &schema.Provider{
+	p := &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			// Why is the key/value named the way they are?
+			"azuredevops_foo": resourceFoo(),
+		},
 		Schema: map[string]*schema.Schema{
 			"org_service_url": {
 				Type:        schema.TypeString,
@@ -34,10 +43,63 @@ func Provider() *schema.Provider {
 				Description: "The personal access token which should be used.",
 			},
 		},
-		ResourcesMap: map[string]*schema.Resource{
+	}
 
-			// Why is the key/value named the way they are?
-			"azuredevops_foo": resourceFoo(),
-		},
+	p.ConfigureFunc = providerConfigure(p)
+
+	return p
+}
+
+func providerConfigure(p *schema.Provider) schema.ConfigureFunc {
+	return func(d *schema.ResourceData) (interface{}, error) {
+		type AzdoConfig struct {
+			azdoPAT         string
+			organizationURL string
+			ctx             context.Context
+		}
+
+		azdoConfig := &AzdoConfig{
+			azdoPAT:         d.Get("personal_access_token").(string),
+			organizationURL: d.Get("org_service_url").(string),
+			ctx:             context.Background(),
+		}
+
+		if azdoConfig.azdoPAT == "" {
+			return nil, fmt.Errorf("the personal access token is required")
+		}
+
+		if azdoConfig.organizationURL == "" {
+			return nil, fmt.Errorf("the url of the Azure DevOps is required")
+		}
+
+		connection := azuredevops.NewPatConnection(azdoConfig.organizationURL, azdoConfig.azdoPAT)
+
+		// client for these APIs (includes CRUD for AzDO projects...):
+		//	https://docs.microsoft.com/en-us/rest/api/azure/devops/core/?view=azure-devops-rest-5.1
+		coreClient, err := core.NewClient(azdoConfig.ctx, connection)
+		if err != nil {
+			return nil, err
+		}
+
+		// client for these APIs (includes CRUD for AzDO build pipelines...):
+		//	https://docs.microsoft.com/en-us/rest/api/azure/devops/build/?view=azure-devops-rest-5.1
+		buildClient, err := build.NewClient(azdoConfig.ctx, connection)
+		if err != nil {
+			return nil, err
+		}
+
+		// client for these APIs (monitor async operations...):
+		//	https://docs.microsoft.com/en-us/rest/api/azure/devops/operations/operations?view=azure-devops-rest-5.1
+		operationsClient := operations.NewClient(azdoConfig.ctx, connection)
+
+		aggregatedClient := &aggregatedClient{
+			CoreClient:       coreClient,
+			BuildClient:      buildClient,
+			OperationsClient: operationsClient,
+			ctx:              azdoConfig.ctx,
+		}
+
+		log.Printf("Created clients successfully!")
+		return aggregatedClient, nil
 	}
 }
