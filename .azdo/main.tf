d1f0e78d3dc11c28b326508678d4bb70f5555490
Nicholas M. Iodice <niiodice@microsoft.com>
2019-10-10 10:59:48 -0500

    Adding nightly build; Removing full OS coverage from PR build; Update .azdo/main.tf for nightly build (#81)

--- .azdo/main.tf
+++ .azdo/main.tf
@@ -16,19 +16,19 @@ resource "azuredevops_serviceendpoint" "github_serviceendpoint" {
   service_endpoint_name  = "GitHub Service Connection"
   service_endpoint_type  = "github"
   service_endpoint_url   = "http://github.com"
-  service_endpoint_owner = "library"
+  service_endpoint_owner = "Library"
 }
 
-resource "azuredevops_build_definition" "build_definition" {
+resource "azuredevops_build_definition" "nightly_build" {
   project_id      = azuredevops_project.project.id
   agent_pool_name = "Hosted Ubuntu 1604"
-  name            = "Provider CI Pipeline"
+  name            = "Nightly Build"
 
   repository {
     repo_type             = "GitHub"
     repo_name             = "microsoft/terraform-provider-azuredevops"
     branch_name           = "master"
-    yml_path              = ".azdo/azure-pipeline.yml"
+    yml_path              = ".azdo/azure-pipeline-nightly.yml"
     service_connection_id = azuredevops_serviceendpoint.github_serviceendpoint.id
   }
 }
