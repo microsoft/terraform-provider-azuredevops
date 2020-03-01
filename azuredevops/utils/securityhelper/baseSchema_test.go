38f58a9b216ab1ccddee261d6b421c7fa3ed2af7
Thomas Meckel <tmeckel@users.noreply.github.com>
2020-03-01 17:09:34 +0100

    Created schema helper function CreatePermissionResourceSchema azuredevops\utils\securityhelper\baseSchema.go and corresponding test

---
+++ azuredevops/utils/securityhelper/baseSchema_test.go
@@ -0,0 +1,50 @@
+// +build all utils securitynamespaces
+
+package securityhelper
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/validate"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreatePermissionResourceSchema(t *testing.T) {
+	schema := CreatePermissionResourceSchema(map[string]*schema.Schema{
+		"project_id": {
+			Type:         schema.TypeString,
+			ValidateFunc: validate.UUID,
+			Required:     true,
+			ForceNew:     true,
+		},
+		"repository_id": {
+			Type:         schema.TypeString,
+			ValidateFunc: validate.UUID,
+			Optional:     true,
+			ForceNew:     true,
+		},
+		"branch_name": {
+			Type:         schema.TypeString,
+			ValidateFunc: validate.NoEmptyStrings,
+			Optional:     true,
+			ForceNew:     true,
+			AllOf:        []string{"repository_id"},
+		},
+	})
+
+	requiredFields := []string{
+		"principal",
+		"replace",
+		"permissions",
+		"project_id",
+		"repository_id",
+		"branch_name",
+	}
+
+	for _, field := range requiredFields {
+		_, ok := schema[field]
+		assert.True(t, ok, fmt.Sprintf("Schema should contain a field [%s]", field))
+	}
+}
