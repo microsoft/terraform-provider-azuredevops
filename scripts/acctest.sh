0daa9b7923d2a564874b2107975c1a5aab7ae72d
Thomas Meckel <tmeckel@users.noreply.github.com>
2020-06-28 13:17:48 +0000

    Excluding all other folders except the acceptancetests folder while running acceptance tests in scripts/acctest.sh

--- scripts/acctest.sh
+++ scripts/acctest.sh
@@ -16,5 +16,5 @@ info "Executing acceptance tests"
     #   - A `-run` parameter is used to target *only* tests starting with `TestAcc`. This prefix is
     #     recommended by Hashicorp and is documented here:
     #       https://www.terraform.io/docs/extend/testing/acceptance-tests/index.html#test-files
-    TF_ACC=1 go test -timeout 120m -run ^TestAcc -tags "${*:-all}" -v ./... || fatal "Build finished in error due to failed tests"
+    TF_ACC=1 go test -timeout 120m -run ^TestAcc -tags "${*:-all}" -v $(go list ./... | grep acceptancetests) || fatal "Build finished in error due to failed tests"
 )
