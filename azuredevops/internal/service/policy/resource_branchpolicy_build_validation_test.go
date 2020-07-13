df05e8e879dd6cd6c1f7f870414e62c4aaa04c6f
Eliise S <elseling@microsoft.com>
2020-07-13 16:31:57 +0000

    Add expand flatten test for build validation policy

---
+++ azuredevops/internal/service/policy/resource_branchpolicy_build_validation_test.go
@@ -0,0 +1,52 @@
+// +build all policy
+// +build !exlcude_policy
+
+package policy
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/stretchr/testify/require"
+
+	"github.com/google/uuid"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/policy"
+	"github.com/terraform-providers/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
+)
+
+// verifies that the flatten/expand round trip path produces repeatable results
+func TestBranchPolicyBuildValidation_ExpandFlatten_Roundtrip(t *testing.T) {
+	var projectID = uuid.New().String()
+	var randomUUID = uuid.New()
+	var testPolicy = &policy.PolicyConfiguration{
+		Id:         converter.Int(1),
+		IsEnabled:  converter.Bool(true),
+		IsBlocking: converter.Bool(true),
+		Type: &policy.PolicyTypeRef{
+			Id: &randomUUID,
+		},
+		Settings: map[string]interface{}{
+			"scope": []map[string]interface{}{
+				{
+					"repositoryId": "test-repo-id",
+					"refName":      "test-ref-name",
+					"matchKind":    "test-match-kind",
+				},
+			},
+			"buildDefinitionId":       77,
+			"displayName":             "Moo",
+			"manualQueueOnly":         true,
+			"queueOnSourceUpdateOnly": true,
+			"validDuration":           700,
+		},
+	}
+
+	resourceData := schema.TestResourceDataRaw(t, ResourceBranchPolicyBuildValidation().Schema, nil)
+	err := buildValidationFlattenFunc(resourceData, testPolicy, &projectID)
+	require.Nil(t, err)
+	expandedPolicy, expandedProjectID, err := buildValidationExpandFunc(resourceData, randomUUID)
+	require.Nil(t, err)
+
+	require.Equal(t, testPolicy, expandedPolicy)
+	require.Equal(t, projectID, *expandedProjectID)
+}
