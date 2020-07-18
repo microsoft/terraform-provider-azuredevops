d214b240b95dfab71752d12b227a6ffc5ba6d217
Thomas Meckel <tmeckel@users.noreply.github.com>
2020-07-18 18:24:14 +0000

    Updated datasource azuredevops_iteration in azuredevops/internal/service/workitemtracking/data_iteration.go

--- azuredevops/internal/service/workitemtracking/data_iteration.go
+++ azuredevops/internal/service/workitemtracking/data_iteration.go
@@ -2,6 +2,7 @@ package workitemtracking
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
@@ -22,7 +23,49 @@ func DataIteration() *schema.Resource {
 			"path": {
 				Type:         schema.TypeString,
 				Optional:     true,
+				Computed:     true,
 				ValidateFunc: validation.StringIsNotWhiteSpace,
+			},
+			"fetch_children": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"has_children": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"children": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"project_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"path": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"has_children": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+					},
+				},
 			},
 		},
 	}
@@ -31,16 +74,19 @@ func DataIteration() *schema.Resource {
 func dataSourceIterationRead(d *schema.ResourceData, m interface{}) error {
 	clients := m.(*client.AggregatedClient)
 	projectID := d.Get("project_id").(string)
-
+	depth := 0
+	if d.Get("fetch_children").(bool) {
+		depth = 1
+	}
 	args := workitemtracking.GetClassificationNodeArgs{
 		Project:        &projectID,
 		StructureGroup: &workitemtracking.TreeStructureGroupValues.Iterations,
-		Depth:          converter.Int(1),
+		Depth:          converter.Int(depth),
 	}
 
-	path, ok := d.GetOk("path")
-	if ok {
-		args.Path = converter.String(path.(string))
+	path, pathSet := d.GetOk("path")
+	if pathSet {
+		args.Path = converter.String(strings.TrimSpace(path.(string)))
 	}
 
 	iteration, err := clients.WorkItemTrackingClient.GetClassificationNode(clients.Ctx, args)
@@ -49,5 +95,48 @@ func dataSourceIterationRead(d *schema.ResourceData, m interface{}) error {
 	}
 
 	d.SetId(iteration.Identifier.String())
+	if args.Path == nil {
+		d.Set("path", convertIterationPath(iteration.Path))
+	}
+	d.Set("name", iteration.Name)
+	d.Set("has_children", converter.ToBool(iteration.HasChildren, false))
+	d.Set("children", flattenIterationNodes(projectID, iteration.Children))
 	return nil
+}
+
+func flattenIterationNodes(projectID string, nodes *[]workitemtracking.WorkItemClassificationNode) []interface{} {
+	if nodes == nil {
+		return nil
+	}
+
+	results := make([]interface{}, len(*nodes))
+	for i, v := range *nodes {
+		results[i] = flattenIterationNode(projectID, v)
+	}
+	return results
+}
+
+func flattenIterationNode(projectID string, node workitemtracking.WorkItemClassificationNode) map[string]interface{} {
+	output := make(map[string]interface{})
+
+	output["id"] = node.Identifier.String()
+	if node.Name != nil {
+		output["name"] = *node.Name
+	}
+	output["project_id"] = projectID
+	output["path"] = convertIterationPath(node.Path)
+	output["has_children"] = converter.ToBool(node.HasChildren, false)
+
+	return output
+}
+
+func convertIterationPath(path *string) string {
+	itemPath := ""
+	if path != nil {
+		itemPathList := strings.Split(strings.ReplaceAll(*path, "\\", "/"), "/")
+		if len(itemPathList) >= 3 {
+			itemPath = strings.Join(itemPathList[3:], "/")
+		}
+	}
+	return "/" + itemPath
 }
