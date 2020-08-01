cb34f695a05df78c5cd3650122b1c51cfbc4a4db
Thomas Meckel <tmeckel@users.noreply.github.com>
2020-08-01 15:15:56 +0000

    Updated implementation of area permissions in azuredevops/internal/service/permissions/resource_area_permissions.go

--- azuredevops/internal/service/permissions/resource_area_permissions.go
+++ azuredevops/internal/service/permissions/resource_area_permissions.go
@@ -2,25 +2,20 @@ package permissions
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"strings"
 
-	"github.com/ahmetb/go-linq"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/workitemtracking"
 	"github.com/terraform-providers/terraform-provider-azuredevops/azuredevops/internal/client"
 	securityhelper "github.com/terraform-providers/terraform-provider-azuredevops/azuredevops/internal/service/permissions/utils"
-	"github.com/terraform-providers/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
 )
 
 func ResourceAreaPermissions() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceAreaPermissionsCreate,
+		Create: resourceAreaPermissionsCreateOrUpdate,
 		Read:   resourceAreaPermissionsRead,
-		Update: resourceAreaPermissionsUpdate,
-		Delete: resourceAREAPermissionsDelete,
+		Update: resourceAreaPermissionsCreateOrUpdate,
+		Delete: resourceAreaPermissionsDelete,
 		Schema: securityhelper.CreatePermissionResourceSchema(map[string]*schema.Schema{
 			"project_id": {
 				Type:         schema.TypeString,
@@ -38,79 +33,17 @@ func ResourceAreaPermissions() *schema.Resource {
 	}
 }
 
-func getAreaIDbyPath(context context.Context, workitemtrackingClient workitemtracking.Client, projectID string, path string) (*string, error) {
-	area, err := workitemtrackingClient.GetClassificationNode(context, workitemtracking.GetClassificationNodeArgs{
-		Project:        &projectID,
-		Path:           &path,
-		StructureGroup: &workitemtracking.TreeStructureGroupValues.Areas,
-		Depth:          converter.Int(1),
-	})
-	if err != nil {
-		return nil, fmt.Errorf("Error getting Area: %w", err)
-	}
-
-	areaID := area.Identifier.String()
-	return &areaID, nil
-}
-
 func createAreaToken(context context.Context, workitemtrackingClient workitemtracking.Client, d *schema.ResourceData) (*string, error) {
-	const aclTokenPrefix = "vstfs:///Classification/Node/"
-
-	var aclToken string
 	projectID := d.Get("project_id").(string)
-
-	// you have to ommit the path property to get the
-	// root area.
-	rootArea, err := workitemtrackingClient.GetClassificationNode(context, workitemtracking.GetClassificationNodeArgs{
-		Project:        converter.String(projectID),
-		StructureGroup: &workitemtracking.TreeStructureGroupValues.Areas,
-		Depth:          converter.Int(1),
-	})
+	path := d.Get("path").(string)
+	aclToken, err := securityhelper.CreateClassificationNodeSecurityToken(context, workitemtrackingClient, workitemtracking.TreeStructureGroupValues.Areas, projectID, path)
 	if err != nil {
-		return nil, fmt.Errorf("Error getting Area: %+v", err)
+		return nil, err
 	}
-
-	/*
-	 * Token format
-	 * Root area: vstfs:///Classification/Node/<AreaIdentifier>:vstfs:///Classification/Node/f8c5b667-91dd-4fe7-bf23-3138c439d07e"
-	 * 1st child: vstfs:///Classification/Node/<AreaIdentifier>:vstfs:///Classification/Node/<AreaIdentifier>
-	 */
-	aclToken = aclTokenPrefix + rootArea.Identifier.String()
-
-	path, ok := d.GetOk("path")
-	if ok {
-		if !*rootArea.HasChildren {
-			return nil, fmt.Errorf("A path was specified but the root area has no children")
-		} else {
-			// get the id for each area in the provided path
-			// we do this by querying each path element
-			// 0: foo
-			// 1: foo/bar
-			// 3: foo/bar/baz
-			var pathElem []string
-
-			linq.From(strings.Split(path.(string), "/")).
-				Where(func(elem interface{}) bool {
-					return len(elem.(string)) > 0
-				}).
-				ToSlice(&pathElem)
-
-			for i := range pathElem {
-				pathItem := strings.Join(pathElem[:i+1], "/")
-				currID, err := getAreaIDbyPath(context, workitemtrackingClient, projectID, pathItem)
-				if err != nil {
-					return nil, fmt.Errorf("Failed to get ID for area %s, %w", pathItem, err)
-				}
-				aclToken = aclToken + ":" + aclTokenPrefix + *currID
-			}
-		}
-	}
-
-	log.Printf("[DEBUG] createAreaToken(): Discovered aclToken %q", aclToken)
 	return &aclToken, nil
 }
 
-func resourceAreaPermissionsCreate(d *schema.ResourceData, m interface{}) error {
+func resourceAreaPermissionsCreateOrUpdate(d *schema.ResourceData, m interface{}) error {
 	clients := m.(*client.AggregatedClient)
 
 	sn, err := securityhelper.NewSecurityNamespace(clients.Ctx,
@@ -160,11 +93,7 @@ func resourceAreaPermissionsRead(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
 
-func resourceAreaPermissionsUpdate(d *schema.ResourceData, m interface{}) error {
-	return resourceAreaPermissionsCreate(d, m)
-}
-
-func resourceAREAPermissionsDelete(d *schema.ResourceData, m interface{}) error {
+func resourceAreaPermissionsDelete(d *schema.ResourceData, m interface{}) error {
 	clients := m.(*client.AggregatedClient)
 
 	aclToken, err := createAreaToken(clients.Ctx, clients.WorkItemTrackingClient, d)
