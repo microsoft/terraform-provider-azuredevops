24caa89460972f7be156afc39791328daa93ebbc
xuzhang3 <57888764+xuzhang3@users.noreply.github.com>
2020-08-04 16:29:27 +0800

    update build definition when remote agent pool is delete and recreate with the same name

--- azuredevops/internal/service/build/resource_build_definition.go
+++ azuredevops/internal/service/build/resource_build_definition.go
@@ -323,10 +323,16 @@ func flattenBuildDefinition(d *schema.ResourceData, buildDefinition *build.Build
 	d.SetId(strconv.Itoa(*buildDefinition.Id))
 
 	d.Set("project_id", projectID)
-	d.Set("name", *buildDefinition.Name)
-	d.Set("path", *buildDefinition.Path)
+	d.Set("name", buildDefinition.Name)
+	d.Set("path", buildDefinition.Path)
 	d.Set("repository", flattenRepository(buildDefinition))
-	d.Set("agent_pool_name", *buildDefinition.Queue.Pool.Name)
+
+	pool := buildDefinition.Queue.Pool
+	if pool != nil {
+		d.Set("agent_pool_name", pool.Name)
+	} else {
+		d.Set("agent_pool_name", "") //trick for state diff
+	}
 
 	d.Set("variable_groups", flattenVariableGroups(buildDefinition))
 	d.Set(bdVariable, flattenBuildVariables(d, buildDefinition))
