2de82967dafcf6052d881024c3fbbec9d1d5f77d
Thomas Meckel <tmeckel@users.noreply.github.com>
2020-08-17 08:44:29 +0000

    Added support for TF v0.13 provider installation location

--- scripts/commons.sh
+++ scripts/commons.sh
@@ -8,6 +8,19 @@ SOURCE_DIR="${SCRIPTS_DIR}/../"
 PROVIDER_NAME_FILE="${SCRIPTS_DIR}/../PROVIDER_NAME.txt"
 PROVIDER_VERSION_FILE="${SCRIPTS_DIR}/../PROVIDER_VERSION.txt"
 
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+if [ "$OS" = "linux" ]; then
+  PROC=$(lscpu 2> /dev/null | awk '/Architecture/ {if($2 == "x86_64") {print "amd64"; exit} else if($2 ~ /arm/) {print "arm"; exit} else if($2 ~ /aarch64/) {print "arm"; exit} else {print "386"; exit}}')
+  if [ -z $PROC ]; then
+    PROC=$(cat /proc/cpuinfo | awk '/model\ name/ {if($0 ~ /ARM/) {print "arm"; exit}}')
+  fi
+  if [ -z $PROC ]; then
+    PROC=$(cat /proc/cpuinfo | awk '/flags/ {if($0 ~ /lm/) {print "amd64"; exit} else {print "386"; exit}}')
+  fi
+else
+  PROC="amd64"
+fi
+[ "$(echo "$PROC" | grep 'arm')" != '' ] && PROC='arm'  # terraform downloads use "arm" not full arm type
 
 function log() {
     LEVEL="$1"
--- scripts/local-install.sh
+++ scripts/local-install.sh
@@ -3,9 +3,20 @@ set -euo pipefail
 
 . $(dirname $0)/commons.sh
 
-
 PLUGINS_DIR="$HOME/.terraform.d/plugins/"
 mkdir -p "$PLUGINS_DIR"
 
 info "Installing provider to $PLUGINS_DIR"
+cp -v "$BUILD_DIR"* "$PLUGINS_DIR/"
+
+## Terraform >= v0.13 requires different layout
+PROVIDER_NAME=$(cat "$PROVIDER_NAME_FILE")
+PROVIDER_VERSION=$(cat "$PROVIDER_VERSION_FILE")
+PROVIDER_REGISTRY='registry.terraform.io'
+PROVIDER_ORGANIZATION='terraform-providers'
+PROVIDER_SOURCE_ADDRESS="${PROVIDER_ORGANIZATION}/${PROVIDER_NAME}"
+
+PLUGINS_DIR="${PLUGINS_DIR}${PROVIDER_REGISTRY}/${PROVIDER_SOURCE_ADDRESS}/${PROVIDER_VERSION}/${OS}_${PROC}"
+info "Installing provider to $PLUGINS_DIR"
+mkdir -p "$PLUGINS_DIR"
 cp -v "$BUILD_DIR"* "$PLUGINS_DIR/"
