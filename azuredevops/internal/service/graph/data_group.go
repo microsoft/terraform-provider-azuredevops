b88d971b529143f2bda4b01d8d74ef994fb352de
Kalarrs Topham <ktopham@opentechalliance.com>
2020-09-08 13:01:43 -0700

    adding data for agent queue & adding origin id to group data

--- azuredevops/internal/service/graph/data_group.go
+++ azuredevops/internal/service/graph/data_group.go
@@ -33,6 +33,10 @@ func DataGroup() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"origin_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -66,6 +70,9 @@ func dataSourceGroupRead(d *schema.ResourceData, m interface{}) error {
 
 	d.SetId(*targetGroup.Descriptor)
 	d.Set("descriptor", *targetGroup.Descriptor)
+	if targetGroup.OriginId != nil {
+		d.Set("origin_id", *targetGroup.OriginId)
+	}
 	return nil
 }
 
