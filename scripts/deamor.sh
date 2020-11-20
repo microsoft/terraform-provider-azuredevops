6a79d3336eac21b91191d631a9f1b7b00c2cff6c
xuzhang3 <Zhangxu894765>
2020-11-20 19:20:33 +0800

    dearmor gpg signature file

---
+++ scripts/deamor.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo 'Dearmor GPG Signature'
+file $(System.DefaultWorkingDirectory)/bin/ADOPackages/*
+cd $(System.DefaultWorkingDirectory)/bin/ADOPackages/
+
+FILE_NAME=""
+files=$(ls)
+
+for filename in $files; do
+  echo "$filename"
+  if [ "${filename##*.}"x = "sig"x ]; then
+    echo "Found signature file"
+    FILE_NAME="$filename"
+    break
+  fi
+done
+
+if [ ! "${FILE_NAME}" ]; then
+  echo "Signature file not found"
+  exit 1
+fi
+
+#cat "${FILE_NAME}"
+cp "${FILE_NAME}" "${FILE_NAME}.bak"
+gpg --dearmor "${FILE_NAME}"
+mv "${FILE_NAME}.gpg" "${FILE_NAME}"
+#rm "${FILE_NAME}"
+
+echo "Print file info"
+ls -al
