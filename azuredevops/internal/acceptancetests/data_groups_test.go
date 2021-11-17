c30dec299cca9d92d2b7a548cd802f3b2d54d946
Thomas Meckel <tmeckel@users.noreply.github.com>
2021-11-17 15:35:00 +0000

    Implemented multi-value data source groups

---
+++ azuredevops/internal/acceptancetests/data_groups_test.go
@@ -0,0 +1,66 @@
+//go:build (all || core || data_sources || data_groups) && (!exclude_data_sources || !exclude_data_groups)
+// +build all core data_sources data_groups
+// +build !exclude_data_sources !exclude_data_groups
+
+package acceptancetests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/acceptancetests/testutils"
+)
+
+func HclGroupsDataSource(projectName string) string {
+	if projectName == "" {
+		return `
+data "azuredevops_groups" "groups" {
+}`
+	}
+	dataSource := `
+data "azuredevops_groups" "groups" {
+	project_id = azuredevops_project.project.id
+}`
+
+	projectResource := testutils.HclProjectResource(projectName)
+	return fmt.Sprintf("%s\n%s", projectResource, dataSource)
+}
+
+// Validates that a configuration containing a project group lookup is able to read the resource correctly.
+// Because this is a data source, there are no resources to inspect in AzDO
+func TestAccGroupsDataSource_Read_Project(t *testing.T) {
+	projectName := testutils.GenerateResourceName()
+	tfNode := "data.azuredevops_groups.groups"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testutils.PreCheck(t, nil) },
+		Providers: testutils.GetProviders(),
+		Steps: []resource.TestStep{
+			{
+				Config: HclGroupsDataSource(projectName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "project_id"),
+					resource.TestCheckResourceAttrSet(tfNode, "groups"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccGroupsDataSource_Read_NoProject(t *testing.T) {
+	tfNode := "data.azuredevops_group.group"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testutils.PreCheck(t, nil) },
+		Providers: testutils.GetProviders(),
+		Steps: []resource.TestStep{
+			{
+				Config: HclGroupsDataSource(""),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "groups"),
+				),
+			},
+		},
+	})
+}
