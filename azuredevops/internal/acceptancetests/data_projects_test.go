02efc73ea63a98c9689c30181cf1fb5bf2c46592
xuzhang3 <Zhangxu894765>
2022-01-25 13:26:56 +0800

    update project

--- azuredevops/internal/acceptancetests/data_projects_test.go
+++ azuredevops/internal/acceptancetests/data_projects_test.go
@@ -17,8 +17,8 @@ func TestAccAzureDevOpsProjects_DataSource_SingleProject(t *testing.T) {
 
 	tfNode := "data.azuredevops_projects.project-list"
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:  func() { testutils.PreCheck(t, nil) },
-		Providers: testutils.GetProviders(),
+		PreCheck:          func() { testutils.PreCheck(t, nil) },
+		ProviderFactories: testutils.GetProviderFactories(),
 		Steps: []resource.TestStep{
 			{
 				Config: projectData,
