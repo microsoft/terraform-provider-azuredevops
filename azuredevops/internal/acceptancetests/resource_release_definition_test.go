e2c3e579f403867cdc489b76d17e08bce4739261
Kalarrs Topham <k-dawg-klark@hotmail.com>
2022-06-03 22:52:20 -0700

    fix imports for test

--- azuredevops/internal/acceptancetests/resource_release_definition_test.go
+++ azuredevops/internal/acceptancetests/resource_release_definition_test.go
@@ -6,10 +6,12 @@ package acceptancetests
 
 import (
 	"fmt"
-	"github.com/microsoft/azure-devops-go-api/azuredevops/release"
 	"strconv"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v6/release"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/acceptancetests/testutils"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
 )
