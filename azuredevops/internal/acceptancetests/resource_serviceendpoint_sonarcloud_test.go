13b88fa3484142f27df6d51d451e3419b5b92806
martenvd <marten.van.dijk@mosadex-ehealth.nl>
2022-10-26 14:36:02 +0200

    azuredevops_serviceendpoint_sonarcloud

---
+++ azuredevops/internal/acceptancetests/resource_serviceendpoint_sonarcloud_test.go
@@ -0,0 +1,173 @@
+//go:build (all || resource_serviceendpoint_SonarCloud) && !exclude_serviceendpoints
+// +build all resource_serviceendpoint_SonarCloud
+// +build !exclude_serviceendpoints
+
+package acceptancetests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/acceptancetests/testutils"
+)
+
+func TestAccServiceEndpointSonarCloud_basic(t *testing.T) {
+	projectName := testutils.GenerateResourceName()
+	serviceEndpointName := testutils.GenerateResourceName()
+
+	resourceType := "azuredevops_serviceendpoint_sonarcloud"
+	tfSvcEpNode := resourceType + ".test"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testutils.PreCheck(t, nil) },
+		Providers:    testutils.GetProviders(),
+		CheckDestroy: testutils.CheckServiceEndpointDestroyed(resourceType),
+		Steps: []resource.TestStep{
+			{
+				Config: hclSvcEndpointSonarCloudResourceBasic(projectName, serviceEndpointName),
+				Check: resource.ComposeTestCheckFunc(
+					testutils.CheckServiceEndpointExistsWithName(tfSvcEpNode, serviceEndpointName),
+					resource.TestCheckResourceAttrSet(tfSvcEpNode, "project_id"),
+					resource.TestCheckResourceAttr(tfSvcEpNode, "service_endpoint_name", serviceEndpointName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccServiceEndpointSonarCloud_complete(t *testing.T) {
+	projectName := testutils.GenerateResourceName()
+	serviceEndpointName := testutils.GenerateResourceName()
+	description := testutils.GenerateResourceName()
+
+	resourceType := "azuredevops_serviceendpoint_sonarcloud"
+	tfSvcEpNode := resourceType + ".test"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testutils.PreCheck(t, nil) },
+		Providers:    testutils.GetProviders(),
+		CheckDestroy: testutils.CheckServiceEndpointDestroyed(resourceType),
+		Steps: []resource.TestStep{
+			{
+				Config: hclSvcEndpointSonarCloudResourceComplete(projectName, serviceEndpointName, description),
+				Check: resource.ComposeTestCheckFunc(
+					testutils.CheckServiceEndpointExistsWithName(tfSvcEpNode, serviceEndpointName),
+					resource.TestCheckResourceAttrSet(tfSvcEpNode, "project_id"),
+					resource.TestCheckResourceAttrSet(tfSvcEpNode, "token_hash"),
+					resource.TestCheckResourceAttr(tfSvcEpNode, "service_endpoint_name", serviceEndpointName),
+					resource.TestCheckResourceAttr(tfSvcEpNode, "description", description),
+				),
+			},
+		},
+	})
+}
+
+func TestAccServiceEndpointSonarCloud_update(t *testing.T) {
+	projectName := testutils.GenerateResourceName()
+	serviceEndpointNameFirst := testutils.GenerateResourceName()
+
+	description := testutils.GenerateResourceName()
+	serviceEndpointNameSecond := testutils.GenerateResourceName()
+
+	resourceType := "azuredevops_serviceendpoint_sonarcloud"
+	tfSvcEpNode := resourceType + ".test"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testutils.PreCheck(t, nil) },
+		Providers:    testutils.GetProviders(),
+		CheckDestroy: testutils.CheckServiceEndpointDestroyed(resourceType),
+		Steps: []resource.TestStep{
+			{
+				Config: hclSvcEndpointSonarCloudResourceBasic(projectName, serviceEndpointNameFirst),
+				Check: resource.ComposeTestCheckFunc(
+					testutils.CheckServiceEndpointExistsWithName(tfSvcEpNode, serviceEndpointNameFirst), resource.TestCheckResourceAttrSet(tfSvcEpNode, "project_id"),
+					resource.TestCheckResourceAttr(tfSvcEpNode, "service_endpoint_name", serviceEndpointNameFirst),
+				),
+			},
+			{
+				Config: hclSvcEndpointSonarCloudResourceUpdate(projectName, serviceEndpointNameSecond, description),
+				Check: resource.ComposeTestCheckFunc(
+					testutils.CheckServiceEndpointExistsWithName(tfSvcEpNode, serviceEndpointNameSecond),
+					resource.TestCheckResourceAttrSet(tfSvcEpNode, "project_id"),
+					resource.TestCheckResourceAttrSet(tfSvcEpNode, "token_hash"),
+					resource.TestCheckResourceAttr(tfSvcEpNode, "service_endpoint_name", serviceEndpointNameSecond),
+					resource.TestCheckResourceAttr(tfSvcEpNode, "description", description),
+				),
+			},
+		},
+	})
+}
+
+func TestAccServiceEndpointSonarCloud_RequiresImportErrorStep(t *testing.T) {
+	projectName := testutils.GenerateResourceName()
+	serviceEndpointName := testutils.GenerateResourceName()
+	resourceType := "azuredevops_serviceendpoint_sonarcloud"
+	tfSvcEpNode := resourceType + ".test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testutils.PreCheck(t, nil) },
+		Providers:    testutils.GetProviders(),
+		CheckDestroy: testutils.CheckServiceEndpointDestroyed(resourceType),
+		Steps: []resource.TestStep{
+			{
+				Config: hclSvcEndpointSonarCloudResourceBasic(projectName, serviceEndpointName),
+				Check: resource.ComposeTestCheckFunc(
+					testutils.CheckServiceEndpointExistsWithName(tfSvcEpNode, serviceEndpointName),
+				),
+			},
+			{
+				Config:      hclSvcEndpointSonarCloudResourceRequiresImport(projectName, serviceEndpointName),
+				ExpectError: testutils.RequiresImportError(serviceEndpointName),
+			},
+		},
+	})
+}
+
+func hclSvcEndpointSonarCloudResourceBasic(projectName string, serviceEndpointName string) string {
+	serviceEndpointResource := fmt.Sprintf(`
+resource "azuredevops_serviceendpoint_sonarcloud" "test" {
+	project_id             = azuredevops_project.project.id
+	service_endpoint_name  = "%s"
+	token			   	   = "redacted"
+}`, serviceEndpointName)
+
+	projectResource := testutils.HclProjectResource(projectName)
+	return fmt.Sprintf("%s\n%s", projectResource, serviceEndpointResource)
+}
+
+func hclSvcEndpointSonarCloudResourceComplete(projectName string, serviceEndpointName string, description string) string {
+	serviceEndpointResource := fmt.Sprintf(`
+resource "azuredevops_serviceendpoint_sonarcloud" "test" {
+	project_id             = azuredevops_project.project.id
+	service_endpoint_name  = "%s"
+	description            = "%s"
+	token			   	   = "redacted"
+}`, serviceEndpointName, description)
+
+	projectResource := testutils.HclProjectResource(projectName)
+	return fmt.Sprintf("%s\n%s", projectResource, serviceEndpointResource)
+}
+
+func hclSvcEndpointSonarCloudResourceUpdate(projectName string, serviceEndpointName string, description string) string {
+	serviceEndpointResource := fmt.Sprintf(`
+resource "azuredevops_serviceendpoint_sonarcloud" "test" {
+	project_id             = azuredevops_project.project.id
+	service_endpoint_name  = "%s"
+	description            = "%s"
+	token			   	   = "redacted2"
+}`, serviceEndpointName, description)
+
+	projectResource := testutils.HclProjectResource(projectName)
+	return fmt.Sprintf("%s\n%s", projectResource, serviceEndpointResource)
+}
+
+func hclSvcEndpointSonarCloudResourceRequiresImport(projectName string, serviceEndpointName string) string {
+	template := hclSvcEndpointSonarCloudResourceBasic(projectName, serviceEndpointName)
+	return fmt.Sprintf(`
+%s
+resource "azuredevops_serviceendpoint_sonarcloud" "import" {
+  project_id                = azuredevops_serviceendpoint_sonarcloud.test.project_id
+  service_endpoint_name = azuredevops_serviceendpoint_sonarcloud.test.service_endpoint_name
+  description            = azuredevops_serviceendpoint_sonarcloud.test.description
+  token          = "redacted"
+}
+`, template)
+}
---
+++ azuredevops/internal/service/serviceendpoint/resource_serviceendpoint_sonarcloud.go
@@ -0,0 +1,52 @@
+package serviceendpoint
+
+import (
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v6/serviceendpoint"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/tfhelper"
+)
+
+// ResourceServiceEndpointSonarCloud schema and implementation for SonarCloud service endpoint resource
+func ResourceServiceEndpointSonarCloud() *schema.Resource {
+	r := genBaseServiceEndpointResource(flattenServiceEndpointSonarCloud, expandServiceEndpointSonarCloud)
+
+	r.Schema["token"] = &schema.Schema{
+		Type:             schema.TypeString,
+		Required:         true,
+		Sensitive:        true,
+		DiffSuppressFunc: tfhelper.DiffFuncSuppressSecretChanged,
+		ValidateFunc:     validation.StringIsNotWhiteSpace,
+		Description:      "Authentication Token generated through SonarCloud (go to My Account > Security > Generate Tokens)",
+	}
+	// Add a spot in the schema to store the token secretly
+	stSecretHashKey, stSecretHashSchema := tfhelper.GenerateSecreteMemoSchema("token")
+	r.Schema[stSecretHashKey] = stSecretHashSchema
+
+	return r
+}
+
+// Convert internal Terraform data structure to an AzDO data structure
+func expandServiceEndpointSonarCloud(d *schema.ResourceData) (*serviceendpoint.ServiceEndpoint, *uuid.UUID, error) {
+	serviceEndpoint, projectID := doBaseExpansion(d)
+	serviceEndpoint.Authorization = &serviceendpoint.EndpointAuthorization{
+		Scheme: converter.String("UsernamePassword"),
+		Parameters: &map[string]string{
+			"username": d.Get("token").(string),
+		},
+	}
+	serviceEndpoint.Type = converter.String("sonarcloud")
+	serviceEndpoint.Url = converter.String("https://sonarcloud.io")
+	return serviceEndpoint, projectID, nil
+}
+
+// Convert AzDO data structure to internal Terraform data structure
+func flattenServiceEndpointSonarCloud(d *schema.ResourceData, serviceEndpoint *serviceendpoint.ServiceEndpoint, projectID *uuid.UUID) {
+	doBaseFlattening(d, serviceEndpoint, projectID)
+
+	tfhelper.HelpFlattenSecret(d, "token")
+
+	d.Set("token", (*serviceEndpoint.Authorization.Parameters)["username"])
+}
