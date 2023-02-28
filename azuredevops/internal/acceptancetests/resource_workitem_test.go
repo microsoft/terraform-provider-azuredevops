a8a4b7d125077badecacb98b8d301af57da86723
Jörg Weißbecker <joerg.weissbecker@pexon-consulting.de>
2023-02-28 08:15:25 +0000

    splitted tests and added import test

--- azuredevops/internal/acceptancetests/resource_workitem_test.go
+++ azuredevops/internal/acceptancetests/resource_workitem_test.go
@@ -11,10 +11,9 @@ import (
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/acceptancetests/testutils"
 )
 
-func TestAccWorkitem_CreateAndUpdate(t *testing.T) {
+func TestAccWorkitem_Create(t *testing.T) {
 	workitemTitle := testutils.GenerateResourceName()
-	projectNameFirst := testutils.GenerateResourceName()
-	projectNameSecond := testutils.GenerateResourceName()
+	projectName := testutils.GenerateResourceName()
 	tfNode := "azuredevops_workitem.workitem"
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -23,9 +22,9 @@ func TestAccWorkitem_CreateAndUpdate(t *testing.T) {
 		CheckDestroy:      testutils.CheckProjectDestroyed,
 		Steps: []resource.TestStep{
 			{
-				Config: testutils.HclWorkitemResource(projectNameFirst, workitemTitle),
+				Config: testutils.HclWorkitemResource(projectName, workitemTitle),
 				Check: resource.ComposeTestCheckFunc(
-					testutils.CheckProjectExists(projectNameFirst),
+					testutils.CheckProjectExists(projectName),
 					resource.TestCheckResourceAttr(tfNode, "title", workitemTitle),
 					resource.TestCheckResourceAttrSet(tfNode, "project_id"),
 					resource.TestCheckResourceAttr(tfNode, "type", "Issue"),
@@ -34,14 +33,52 @@ func TestAccWorkitem_CreateAndUpdate(t *testing.T) {
 					resource.TestCheckResourceAttr(tfNode, "tags.1", "tag2=value"),
 				),
 			},
+		},
+	})
+}
+
+func TestAccWorkitem_Update(t *testing.T) {
+	workitemTitle := testutils.GenerateResourceName()
+	workitemTitleUpdated := testutils.GenerateResourceName()
+	projectName := testutils.GenerateResourceName()
+	tfNode := "azuredevops_workitem.workitem"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testutils.PreCheck(t, nil) },
+		ProviderFactories: testutils.GetProviderFactories(),
+		CheckDestroy:      testutils.CheckProjectDestroyed,
+		Steps: []resource.TestStep{
 			{
-				Config: testutils.HclWorkitemResource(projectNameSecond, workitemTitle),
+				Config: testutils.HclWorkitemResource(projectName, workitemTitle),
 				Check: resource.ComposeTestCheckFunc(
-					testutils.CheckProjectExists(projectNameSecond),
+					testutils.CheckProjectExists(projectName),
 					resource.TestCheckResourceAttr(tfNode, "title", workitemTitle),
-					resource.TestCheckResourceAttrSet(tfNode, "project_id"),
-					resource.TestCheckResourceAttr(tfNode, "type", "Issue"),
-					resource.TestCheckResourceAttr(tfNode, "state", "Active"),
+				),
+			},
+			{
+				Config: testutils.HclWorkitemResource(projectName, workitemTitleUpdated),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(tfNode, "title", workitemTitleUpdated),
+				),
+			},
+		},
+	})
+}
+
+func TestAccWorkitem_Import(t *testing.T) {
+
+	tfNode := "azuredevops_workitem.workitem"
+	workitemTitle := testutils.GenerateResourceName()
+	projectName := testutils.GenerateResourceName()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testutils.PreCheck(t, nil) },
+		ProviderFactories: testutils.GetProviderFactories(),
+		CheckDestroy:      testutils.CheckProjectDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: testutils.HclWorkitemResource(projectName, workitemTitle),
+				Check: resource.ComposeTestCheckFunc(
+					testutils.CheckProjectExists(projectName),
 				),
 			},
 			{
