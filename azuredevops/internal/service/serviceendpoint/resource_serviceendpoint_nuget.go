cf54a732abe9bffd4483fa22854734f9baa8b127
David Corrigan <47994662+davidcorrigan714@users.noreply.github.com>
2023-03-29 07:47:37 -0500

    remove build comments/commands(?) (#9)

--- azuredevops/internal/service/serviceendpoint/resource_serviceendpoint_nuget.go
+++ azuredevops/internal/service/serviceendpoint/resource_serviceendpoint_nuget.go
@@ -1,7 +1,3 @@
-//go:build (all || resource_serviceendpoint_nuget) && !exclude_serviceendpoints
-// +build all resource_serviceendpoint_nuget
-// +build !exclude_serviceendpoints
-
 package serviceendpoint
 
 import (
