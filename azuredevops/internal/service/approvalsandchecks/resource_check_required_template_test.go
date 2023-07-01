3bca66857feaa7f4f8588b0293e8bbabad249b4a
Rodolfo Albuquerque <rdalbuquerque91@gmail.com>
2023-07-01 14:02:38 -0300

    fix linter errors

--- azuredevops/internal/service/approvalsandchecks/resource_check_required_template_test.go
+++ azuredevops/internal/service/approvalsandchecks/resource_check_required_template_test.go
@@ -20,7 +20,6 @@ import (
 var requiredTemplateCheckID = 123456789
 var requiredTemplateEndpointID = uuid.New().String()
 var requiredTemplateCheckProjectID = uuid.New().String()
-var requiredTemplateTestProjectID = &requiredTemplateCheckProjectID
 
 var requiredTemplateTestEndpointType = "endpoint"
 var requiredTemplateTestEndpointResource = pipelineschecksextras.Resource{
@@ -51,13 +50,14 @@ var requiredTemplateCheckTest = pipelineschecksextras.CheckConfiguration{
 // verifies that the flatten/expand round trip yields the same required template
 func TestCheckRequiredTemplate_ExpandFlatten_Roundtrip(t *testing.T) {
 	resourceData := schema.TestResourceDataRaw(t, ResourceCheckRequiredTemplate().Schema, nil)
-	flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
+	flattenErr := flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
 
-	requiredTemplateCheckAfterRoundTrip, projectID, err := expandCheckRequiredTemplate(resourceData)
+	requiredTemplateCheckAfterRoundTrip, projectID, expandErr := expandCheckRequiredTemplate(resourceData)
 
 	require.Equal(t, requiredTemplateCheckTest, *requiredTemplateCheckAfterRoundTrip)
 	require.Equal(t, requiredTemplateCheckProjectID, projectID)
-	require.Nil(t, err)
+	require.Nil(t, expandErr)
+	require.Nil(t, flattenErr)
 }
 
 // verifies that if an error is produced on create, the error is not swallowed
@@ -67,7 +67,7 @@ func TestCheckRequiredTemplate_Create_DoesNotSwallowError(t *testing.T) {
 
 	r := ResourceCheckRequiredTemplate()
 	resourceData := schema.TestResourceDataRaw(t, r.Schema, nil)
-	flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
+	flattenErr := flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
 
 	pipelinesChecksClient := azdosdkmocks.NewMockPipelineschecksextrasClient(ctrl)
 	clients := &client.AggregatedClient{PipelinesChecksClientExtras: pipelinesChecksClient, Ctx: context.Background()}
@@ -81,6 +81,7 @@ func TestCheckRequiredTemplate_Create_DoesNotSwallowError(t *testing.T) {
 
 	err := r.Create(resourceData, clients)
 	require.Contains(t, err.Error(), "AddCheckConfiguration() Failed")
+	require.Nil(t, flattenErr)
 }
 
 // verifies that if an error is produced on a read, it is not swallowed
@@ -90,7 +91,7 @@ func TestCheckRequiredTemplate_Read_DoesNotSwallowError(t *testing.T) {
 
 	r := ResourceCheckRequiredTemplate()
 	resourceData := schema.TestResourceDataRaw(t, r.Schema, nil)
-	flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
+	flattenErr := flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
 
 	pipelinesChecksClient := azdosdkmocks.NewMockPipelineschecksextrasClient(ctrl)
 	clients := &client.AggregatedClient{PipelinesChecksClientExtras: pipelinesChecksClient, Ctx: context.Background()}
@@ -109,6 +110,7 @@ func TestCheckRequiredTemplate_Read_DoesNotSwallowError(t *testing.T) {
 
 	err := r.Read(resourceData, clients)
 	require.Contains(t, err.Error(), "GetServiceEndpoint() Failed")
+	require.Nil(t, flattenErr)
 }
 
 // verifies that if an error is produced on a delete, it is not swallowed
@@ -118,7 +120,7 @@ func TestCheckRequiredTemplate_Delete_DoesNotSwallowError(t *testing.T) {
 
 	r := ResourceCheckRequiredTemplate()
 	resourceData := schema.TestResourceDataRaw(t, r.Schema, nil)
-	flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
+	flattenErr := flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
 
 	pipelinesChecksClient := azdosdkmocks.NewMockPipelineschecksextrasClient(ctrl)
 	clients := &client.AggregatedClient{PipelinesChecksClientExtras: pipelinesChecksClient, Ctx: context.Background()}
@@ -136,6 +138,7 @@ func TestCheckRequiredTemplate_Delete_DoesNotSwallowError(t *testing.T) {
 
 	err := r.Delete(resourceData, clients)
 	require.Contains(t, err.Error(), "DeleteServiceEndpoint() Failed")
+	require.Nil(t, flattenErr)
 }
 
 // verifies that if an error is produced on an update, it is not swallowed
@@ -145,7 +148,7 @@ func TestCheckRequiredTemplate_Update_DoesNotSwallowError(t *testing.T) {
 
 	r := ResourceCheckRequiredTemplate()
 	resourceData := schema.TestResourceDataRaw(t, r.Schema, nil)
-	flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
+	flattenErr := flattenCheckRequiredTemplate(resourceData, &requiredTemplateCheckTest, requiredTemplateCheckProjectID)
 
 	pipelinesChecksClient := azdosdkmocks.NewMockPipelineschecksextrasClient(ctrl)
 	clients := &client.AggregatedClient{PipelinesChecksClientExtras: pipelinesChecksClient, Ctx: context.Background()}
@@ -164,4 +167,5 @@ func TestCheckRequiredTemplate_Update_DoesNotSwallowError(t *testing.T) {
 
 	err := r.Update(resourceData, clients)
 	require.Contains(t, err.Error(), "UpdateServiceEndpoint() Failed")
+	require.Nil(t, flattenErr)
 }
