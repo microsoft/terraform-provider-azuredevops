79fffa4be628f3e6eab0b519dc871d80b226fecc
Bart van der Braak <bartvdbraak@gmail.com>
2023-07-16 15:18:50 +0000

    feat: added ResourceCheckExclusiveLock

--- azuredevops/internal/service/approvalsandchecks/resource_check_exclusive_lock.go
+++ azuredevops/internal/service/approvalsandchecks/resource_check_exclusive_lock.go
@@ -2,20 +2,38 @@ package approvalsandchecks
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/pipelineschecksextras"
 )
 
-// ResourceCheckExclusiveLock schema and implementation for exclusive lock resource
+var exclusiveLockId = "2EF31AD6-BAA0-403A-8B45-2CBC9B4E5563"
+
+// ResourceCheckExclusiveLock schema and implementation for Exclusive Lock resource
 func ResourceCheckExclusiveLock() *schema.Resource {
-	return genBaseCheckResource(flattenExclusiveLock, expandExclusiveLock)
+	r := genBaseCheckResource(flattenExclusiveLock, expandExclusiveLock)
+
+	r.Schema["display_name"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Optional: true,
+		Default:  "Managed by Terraform",
+	}
+
+	return r
 }
 
 func flattenExclusiveLock(d *schema.ResourceData, exclusiveLockCheck *pipelineschecksextras.CheckConfiguration, projectID string) error {
-	// TODO: implement this function
+	err := doBaseFlattening(d, exclusiveLockCheck, projectID)
+	if err != nil {
+		return err
+	}
+
+	exclusiveLockCheck.Type.Id = converter.UUID(exclusiveLockId)
 	return nil
 }
 
 func expandExclusiveLock(d *schema.ResourceData) (*pipelineschecksextras.CheckConfiguration, string, error) {
-	// TODO: implement this function
-	return nil, "", nil
+	settings := make(map[string]interface{})
+	settings["displayName"] = d.Get("display_name").(string)
+
+	return doBaseExpansion(d, approvalAndCheckType.ExclusiveLock, settings, nil)
 }
