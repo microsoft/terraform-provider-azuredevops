3cedabef02584c7f72aea3094e6a57cc724a6e6e
Bart van der Braak <bartvdbraak@gmail.com>
2023-07-17 00:06:12 +0000

    feat: added tests and timeout

--- azuredevops/internal/service/approvalsandchecks/resource_check_exclusive_lock.go
+++ azuredevops/internal/service/approvalsandchecks/resource_check_exclusive_lock.go
@@ -1,39 +1,47 @@
 package approvalsandchecks
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/pipelineschecksextras"
 )
 
-var exclusiveLockId = "2EF31AD6-BAA0-403A-8B45-2CBC9B4E5563"
-
 // ResourceCheckExclusiveLock schema and implementation for Exclusive Lock resource
 func ResourceCheckExclusiveLock() *schema.Resource {
 	r := genBaseCheckResource(flattenExclusiveLock, expandExclusiveLock)
 
-	r.Schema["display_name"] = &schema.Schema{
-		Type:     schema.TypeString,
+	r.Schema["timeout"] = &schema.Schema{
+		Type:     schema.TypeInt,
 		Optional: true,
-		Default:  "Managed by Terraform",
+		Default:  43200,
 	}
 
 	return r
 }
 
-func flattenExclusiveLock(d *schema.ResourceData, exclusiveLockCheck *pipelineschecksextras.CheckConfiguration, projectID string) error {
-	err := doBaseFlattening(d, exclusiveLockCheck, projectID)
+func flattenExclusiveLock(d *schema.ResourceData, check *pipelineschecksextras.CheckConfiguration, projectID string) error {
+	err := doBaseFlattening(d, check, projectID)
 	if err != nil {
 		return err
 	}
 
-	exclusiveLockCheck.Type.Id = converter.UUID(exclusiveLockId)
+	if check.Settings == nil {
+		return fmt.Errorf("settings nil")
+	}
+
+	check.Type = approvalAndCheckType.ExclusiveLock
+
+	if check.Timeout != nil {
+		d.Set("timeout", *check.Timeout)
+	}
+
 	return nil
 }
 
 func expandExclusiveLock(d *schema.ResourceData) (*pipelineschecksextras.CheckConfiguration, string, error) {
 	settings := make(map[string]interface{})
-	settings["displayName"] = d.Get("display_name").(string)
 
-	return doBaseExpansion(d, approvalAndCheckType.ExclusiveLock, settings, nil)
+	return doBaseExpansion(d, approvalAndCheckType.ExclusiveLock, settings, converter.ToPtr(d.Get("timeout").(int)))
 }
