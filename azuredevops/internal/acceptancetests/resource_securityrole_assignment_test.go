0f64f943797680f6313b7668e3c7d7f09ea8d7f9
Vladimir Lazarenko <favoretti@gmail.com>
2024-03-24 22:04:48 +0100

    imports

--- azuredevops/internal/acceptancetests/resource_securityrole_assignment_test.go
+++ azuredevops/internal/acceptancetests/resource_securityrole_assignment_test.go
@@ -9,12 +9,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/graph"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/acceptancetests/testutils"
-	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
-	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils"
-	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
 )
 
 func TestAccSecurityroleAssignmentResource_Basic(t *testing.T) {
