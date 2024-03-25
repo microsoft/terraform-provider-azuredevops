5c7bc4c8574aa0d882bc750f3c025562cd0c3290
Mateusz Wreczycki <mateusz.wr-czycki.osv@fedex.com>
2024-03-25 16:42:53 +0100

    cover acc tests for Feed Resource

---
+++ azuredevops/internal/acceptancetests/resource_feed_test.go
@@ -0,0 +1,65 @@
+//go:build (all || core || data_sources || data_feed) && (!data_sources || !exclude_feed)
+// +build all core data_sources data_feed
+// +build !data_sources !exclude_feed
+
+package acceptancetests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/acceptancetests/testutils"
+)
+
+func TestAccAzureDevOps_Resource_Feed(t *testing.T) {
+	name := testutils.GenerateResourceName()
+
+	FeedResource := testutils.HclFeedResource(name)
+
+	tfNode := "azuredevops_feed.feed"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testutils.PreCheck(t, nil) },
+		ProviderFactories: testutils.GetProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: FeedResource,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "name"),
+					resource.TestCheckNoResourceAttr(tfNode, "project"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAzureDevOps_Resource_Feed_with_Project(t *testing.T) {
+	name := testutils.GenerateResourceName()
+	projectName := testutils.GenerateResourceName()
+
+	ProjectResource := testutils.HclProjectResource(projectName)
+	FeedResource := fmt.Sprintf(`
+	%s
+
+	resource "azuredevops_feed" "feed" {
+		name       = "%s"
+		project    = azuredevops_project.project.name
+	}
+	
+	`, ProjectResource, name)
+
+	tfNode := "azuredevops_feed.feed"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testutils.PreCheck(t, nil) },
+		ProviderFactories: testutils.GetProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: FeedResource,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "name"),
+					resource.TestCheckResourceAttrSet(tfNode, "project"),
+				),
+			},
+		},
+	})
+}
