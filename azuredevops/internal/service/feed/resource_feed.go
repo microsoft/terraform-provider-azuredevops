84280ff53689b3d943939ba4c174ae98df1f0b44
xu <xuzhang3@microsoft.com>
2024-05-10 15:18:29 +0800

    format

--- azuredevops/internal/service/feed/resource_feed.go
+++ azuredevops/internal/service/feed/resource_feed.go
@@ -2,7 +2,6 @@ package feed
 
 import (
 	"fmt"
-	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
@@ -10,6 +9,7 @@ import (
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/webapi"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
 )
 
 func ResourceFeed() *schema.Resource {
