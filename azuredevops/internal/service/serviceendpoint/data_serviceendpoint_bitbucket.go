089c49589f744187e5554d82b039108e58c65ad4
xu <xuzhang3@microsoft.com>
2024-10-24 10:32:02 +0800

    add support bitbucket data source

---
+++ azuredevops/internal/service/serviceendpoint/data_serviceendpoint_bitbucket.go
@@ -0,0 +1,38 @@
+package serviceendpoint
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func DataResourceServiceEndpointBitbucket() *schema.Resource {
+	resource := &schema.Resource{
+		Read: dataSourceServiceEndpointBitbucketRead,
+		Timeouts: &schema.ResourceTimeout{
+			Read: schema.DefaultTimeout(5 * time.Minute),
+		},
+		Schema: dataSourceGenBaseSchema(),
+	}
+	return resource
+}
+
+func dataSourceServiceEndpointBitbucketRead(d *schema.ResourceData, m interface{}) error {
+	serviceEndpoint, err := dataSourceGetBaseServiceEndpoint(d, m)
+
+	if err != nil {
+		return err
+	}
+
+	if serviceEndpoint != nil {
+		if err = checkServiceConnection(serviceEndpoint); err != nil {
+			return err
+		}
+		doBaseFlattening(d, serviceEndpoint)
+		d.Set("service_endpoint_id", serviceEndpoint.Url)
+		return nil
+	}
+
+	return fmt.Errorf(" Looking up Bitbucket Service Endpoint!")
+}
