24e5e482871f1be039922b11cadd424e6f5a3366
xu <xuzhang3@microsoft.com>
2024-11-25 14:22:48 +0800

    update name without recreate

--- azuredevops/internal/service/serviceendpoint/commons.go
+++ azuredevops/internal/service/serviceendpoint/commons.go
@@ -37,7 +37,6 @@ func baseSchema() map[string]*schema.Schema {
 		"project_id": {
 			Type:         schema.TypeString,
 			Required:     true,
-			ForceNew:     true,
 			ValidateFunc: validation.IsUUID,
 		},
 		"service_endpoint_name": {
