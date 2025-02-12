a815b241995153befd0e575b8cf79e5d53a33047
xu <xuzhang3@microsoft.com>
2025-02-12 11:11:29 +0800

    update error message

--- azuredevops/internal/service/graph/data_user.go
+++ azuredevops/internal/service/graph/data_user.go
@@ -2,6 +2,7 @@ package graph
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -75,7 +76,7 @@ func dataIdentitySourceUserRead(ctx context.Context, d *schema.ResourceData, m i
 		if utils.ResponseWasNotFound(err) {
 			return diag.Errorf(" User does not exist with descriptor: %s", descriptor)
 		}
-		return diag.FromErr(err)
+		return diag.FromErr(fmt.Errorf(" Reading User: %+v", err))
 	}
 
 	if user == nil {
