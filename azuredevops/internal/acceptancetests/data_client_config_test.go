45f6ca9753a1dccb5d936639f87b3a8be4017ae7
xu <xuzhang3@microsoft.com>
2025-02-19 11:26:43 +0800

    add support for exporting organization meta

--- azuredevops/internal/acceptancetests/data_client_config_test.go
+++ azuredevops/internal/acceptancetests/data_client_config_test.go
@@ -13,15 +13,18 @@ import (
 
 // Verifies that the client config data source loads the configured AzDO org
 func TestAccClientConfig_LoadsCorrectProperties(t *testing.T) {
-	tfNode := "data.azuredevops_client_config.c"
+	tfNode := "data.azuredevops_client_config.test"
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:  func() { testutils.PreCheck(t, nil) },
 		Providers: testutils.GetProviders(),
 		Steps: []resource.TestStep{
 			{
-				Config: `data "azuredevops_client_config" "c" {}`,
+				Config: `data "azuredevops_client_config" "test" {}`,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(tfNode, "id"),
+					resource.TestCheckResourceAttrSet(tfNode, "name"),
+					resource.TestCheckResourceAttrSet(tfNode, "organization_id"),
+					resource.TestCheckResourceAttrSet(tfNode, "status"),
+					resource.TestCheckResourceAttrSet(tfNode, "tenant_id"),
 					resource.TestCheckResourceAttr(tfNode, "organization_url", os.Getenv("AZDO_ORG_SERVICE_URL")),
 				),
 			},
--- azuredevops/internal/service/data_client_config.go
+++ azuredevops/internal/service/data_client_config.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"context"
+	"strings"
 	"time"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
 )
@@ -10,12 +13,37 @@ import (
 // DataClientConfig schema and implementation for AzDO client configuration
 func DataClientConfig() *schema.Resource {
 	return &schema.Resource{
-		Read: clientConfigRead,
+		ReadContext: clientConfigRead,
 		Timeouts: &schema.ResourceTimeout{
 			Read: schema.DefaultTimeout(5 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"organization_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
 			"organization_url": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"owner_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"tenant_id": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -23,8 +51,22 @@ func DataClientConfig() *schema.Resource {
 	}
 }
 
-func clientConfigRead(d *schema.ResourceData, m interface{}) error {
-	d.SetId(time.Now().UTC().String())
+func clientConfigRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	clients := m.(*client.AggregatedClient)
+
+	parts := strings.Split(m.(*client.AggregatedClient).OrganizationURL, "/")
+
+	orgMeta, err := clients.OrganizationClient.GetOrganizationMeta(clients.Ctx, parts[3])
+	if err != nil {
+		return diag.Errorf(" Getting organization metadata: %s", err)
+	}
+
+	d.SetId(*orgMeta.Id)
 	d.Set("organization_url", m.(*client.AggregatedClient).OrganizationURL)
+	d.Set("organization_id", orgMeta.Id)
+	d.Set("status", orgMeta.Status)
+	d.Set("name", orgMeta.Name)
+	d.Set("tenant_id", orgMeta.TenantId)
+	d.Set("owner_id", orgMeta.Owner)
 	return nil
 }
