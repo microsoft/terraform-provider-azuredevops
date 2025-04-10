815725a44ef1c43f6280332260c59dac81a0c11a
xuzhang3 <xuzhang3@microsoft.com>
2025-04-10 11:43:42 +0800

    terrafmt

---
+++ scripts/terrafmt.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "==> Checking terraform blocks are formatted..."
+
+files=$(find ./azuredevops -type f -name "*_test.go")
+error=false
+
+for f in $files; do
+  terrafmt diff -c -q -f "$f" || error=true
+done
+
+if ${error}; then
+  echo "------------------------------------------------"
+  echo ""
+  echo "The preceding files contain terraform blocks that are not correctly formatted or contain errors."
+  echo "You can fix this by running 'make tools' and then terrafmt on them."
+  echo ""
+  echo "to easily fix all terraform blocks:"
+  echo "$ make terrafmt"
+  echo ""
+  echo "format a single test file:"
+  echo "$ terrafmt fmt -f ./azuredevops/internal/acceptancetests/data_area_test.go"
+fi
+
+exit 0
