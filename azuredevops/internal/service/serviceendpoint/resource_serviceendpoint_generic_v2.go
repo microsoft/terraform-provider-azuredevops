c18567a7be6ccc80b34210a1d075edcf8a17838e
Levente Laszlo Papp <leventelaszlo.papp@kuka.com>
2025-07-06 20:07:27 +0200

    Reorder import statements for improved organization and clarity

--- azuredevops/internal/service/serviceendpoint/resource_serviceendpoint_generic_v2.go
+++ azuredevops/internal/service/serviceendpoint/resource_serviceendpoint_generic_v2.go
@@ -3,12 +3,12 @@ package serviceendpoint
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
