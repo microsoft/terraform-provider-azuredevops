f61bd82c5ec4537439b0ad9bb5aa989cd1f88453
Fredrik Arvidsson <fresa@fresa.se>
2025-11-17 08:31:27 +0100

    add missing fields for processes

--- azuredevops/internal/service/workitemtrackingprocess/resource_processes.go
+++ azuredevops/internal/service/workitemtrackingprocess/resource_processes.go
@@ -25,33 +25,85 @@ func ResourceProcesses() *schema.Resource {
 			Delete: schema.DefaultTimeout(10 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
-			"type_id": {
-				Type:         schema.TypeString,
-				ForceNew:     true,
-				Required:     true,
-				ValidateFunc: validation.IsUUID,
-				Description:  "The ID of the process",
-			},
 			"name": {
 				Type:         schema.TypeString,
 				Required:     true,
 				ValidateFunc: validation.StringIsNotWhiteSpace,
 				Description:  "Name of the process",
 			},
-			"parent_process_type_id": {
+			"description": {
 				Type:         schema.TypeString,
 				Optional:     true,
+				ValidateFunc: validation.StringIsNotWhiteSpace,
+				Description:  "Description of the process",
+			},
+			"parent_process_type_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
 				ValidateFunc: validation.IsUUID,
 				Description:  "ID of the parent process",
 			},
 			"reference_name": {
 				Type:         schema.TypeString,
 				Optional:     true,
+				Computed:     true,
+				ForceNew:     true,
 				ValidateFunc: validation.StringIsNotWhiteSpace,
-				Elem: &schema.Schema{
-					Type:         schema.TypeString,
-					ValidateFunc: validation.StringIsNotWhiteSpace,
+				Description:  "Reference name of process being created. If not specified, server will assign a unique reference name",
+			},
+			"is_default": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Is the process default?",
+			},
+			"is_enabled": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     true,
+				Description: "Is the process enabled?",
+			},
+			"customization_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Indicates the type of customization on this process. System Process is default process. Inherited Process is modified process that was System process before",
+			},
+			"expand": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "none",
+				ValidateFunc: validation.StringInSlice([]string{"none", "projects"}, false),
+				Description:  "Specifies the expand option when getting the process",
+			},
+			"projects": {
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The ID of the project",
+						},
+						"description": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Description of the project",
+						},
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Name of the project",
+						},
+						"url": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Url of the project",
+						},
+					},
 				},
+				Computed:    true,
+				Description: "Returns associated projects when using the 'projects' expand option",
 			},
 		},
 	}
