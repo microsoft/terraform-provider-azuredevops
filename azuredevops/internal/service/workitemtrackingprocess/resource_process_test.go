032158a2a238bde44cd1595ea34b66b9f72568ec
Fredrik Arvidsson <fresa@fresa.se>
2025-11-19 08:12:59 +0100

    test(process): assert disabling when creating

--- azuredevops/internal/service/workitemtrackingprocess/resource_process_test.go
+++ azuredevops/internal/service/workitemtrackingprocess/resource_process_test.go
@@ -155,6 +155,75 @@ func TestProcesses_CreateWithDefault_Successful(t *testing.T) {
 	assert.Equal(t, 0, projects.Len())
 }
 
+func TestProcesses_CreateWithDisabled_Successful(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockClient := azdosdkmocks.NewMockWorkitemtrackingprocessClient(ctrl)
+	clients := &client.AggregatedClient{WorkItemTrackingProcessClient: mockClient, Ctx: context.Background()}
+
+	parentID := uuid.New()
+	typeID := uuid.New()
+	name := "MyProcess"
+	referenceName := "My.Process.ReferenceName"
+	expectedProcessInfo := &workitemtrackingprocess.ProcessInfo{
+		TypeId:              &typeID,
+		Name:                &name,
+		Description:         converter.String(""),
+		IsDefault:           converter.Bool(false),
+		IsEnabled:           converter.Bool(true),
+		CustomizationType:   &workitemtrackingprocess.CustomizationTypeValues.Inherited,
+		ParentProcessTypeId: &parentID,
+		ReferenceName:       &referenceName,
+	}
+
+	mockClient.EXPECT().CreateNewProcess(clients.Ctx, gomock.Any()).DoAndReturn(
+		func(ctx context.Context, args workitemtrackingprocess.CreateNewProcessArgs) (*workitemtrackingprocess.ProcessInfo, error) {
+			assert.Equal(t, name, *args.CreateRequest.Name)
+			assert.Equal(t, parentID, *args.CreateRequest.ParentProcessTypeId)
+			assert.Nil(t, args.CreateRequest.ReferenceName)
+			return expectedProcessInfo, nil
+		},
+	).Times(1)
+	mockClient.EXPECT().EditProcess(clients.Ctx, gomock.Any()).DoAndReturn(
+		func(ctx context.Context, args workitemtrackingprocess.EditProcessArgs) (*workitemtrackingprocess.ProcessInfo, error) {
+			assert.Equal(t, typeID, *args.ProcessTypeId)
+			assert.Equal(t, name, *args.UpdateRequest.Name)
+			assert.False(t, *args.UpdateRequest.IsDefault)
+			assert.False(t, *args.UpdateRequest.IsEnabled)
+			expectedProcessInfo.IsEnabled = args.UpdateRequest.IsEnabled
+			return expectedProcessInfo, nil
+		},
+	).Times(1)
+
+	mockClient.EXPECT().GetProcessByItsId(clients.Ctx, gomock.Any()).DoAndReturn(
+		func(ctx context.Context, args workitemtrackingprocess.GetProcessByItsIdArgs) (*workitemtrackingprocess.ProcessInfo, error) {
+			assert.Equal(t, typeID, *args.ProcessTypeId)
+			return expectedProcessInfo, nil
+		},
+	).Times(1)
+
+	d := getProcessResourceData(t, map[string]any{
+		"name":                   name,
+		"parent_process_type_id": parentID.String(),
+		"is_enabled":             false,
+	})
+
+	diags := createResourceProcess(context.Background(), d, clients)
+
+	assert.Empty(t, diags)
+	assert.Equal(t, typeID.String(), d.Id())
+	assert.Equal(t, name, d.Get("name"))
+	assert.Equal(t, referenceName, d.Get("reference_name"))
+	assert.Equal(t, parentID.String(), d.Get("parent_process_type_id"))
+	assert.Equal(t, false, d.Get("is_default"))
+	assert.Equal(t, false, d.Get("is_enabled"))
+	assert.Equal(t, "inherited", d.Get("customization_type"))
+	projects := d.Get("projects").(*schema.Set)
+	assert.NotNil(t, projects)
+	assert.Equal(t, 0, projects.Len())
+}
+
 func TestProcesses_Update_Successful(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
