16abd423c5e60c596f5c625b9f6ace0b6ebee488
Jason Ouellet <jasonouellet@users.noreply.github.com>
2025-11-19 14:52:02 -0500

    Merge branch 'microsoft:main' into feature/jfrog_v2_data

---
+++ azuredevops/internal/service/serviceendpoint/data_serviceendpoint_jfrog_distribution_v2.go
@@ -0,0 +1,45 @@
+package serviceendpoint
+
+import (
+	"fmt"
+	"maps"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/validate"
+)
+
+// DataSourceServiceEndpointJFrogDistributionV2 schema and implementation for JFrog Distribution service endpoint resource
+func DataSourceServiceEndpointJFrogDistributionV2() *schema.Resource {
+	r := &schema.Resource{
+		Read: DataSourceServiceEndpointJFrogDistributionV2Read,
+		Timeouts: &schema.ResourceTimeout{
+			Read: schema.DefaultTimeout(1 * time.Minute),
+		},
+		Schema: dataSourceGenBaseSchema(),
+	}
+	maps.Copy(r.Schema, map[string]*schema.Schema{
+		"url": {
+			Type:     schema.TypeString,
+			Computed: true,
+		},
+	})
+
+	return r
+}
+
+func DataSourceServiceEndpointJFrogDistributionV2Read(d *schema.ResourceData, m interface{}) error {
+	serviceEndpoint, err := dataSourceGetBaseServiceEndpoint(d, m)
+	if err != nil {
+		return err
+	}
+
+	if err = checkServiceConnection(serviceEndpoint); err != nil {
+		return err
+	}
+        doBaseFlattening(d, serviceEndpoint)
+	d.Set("url", serviceEndpoint.Url)
+	return nil
+}
