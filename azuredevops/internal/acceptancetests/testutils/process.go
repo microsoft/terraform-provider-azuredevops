77e567490165b247bc69fba700085b7f4686d636
Fredrik Arvidsson <fresa@fresa.se>
2025-11-26 14:31:31 +0100

    test(process): retry read to verify destroy as it is eventual consistent

--- azuredevops/internal/acceptancetests/testutils/process.go
+++ azuredevops/internal/acceptancetests/testutils/process.go
@@ -2,17 +2,21 @@ package testutils
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/core"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils"
 )
 
 // CheckProcessDestroyed verifies that all processes referenced in the state are destroyed. This will be invoked
 // *after* terraform destroys the resource but *before* the state is wiped clean.
 func CheckProcessDestroyed(s *terraform.State) error {
 	clients := GetProvider().Meta().(*client.AggregatedClient)
+	timeout := 10 * time.Second
 
 	// verify that every process referenced in the state does not exist in AzDO
 	for _, resource := range s.RootModule().Resources {
@@ -25,8 +29,19 @@ func CheckProcessDestroyed(s *terraform.State) error {
 			return err
 		}
 
-		if _, err := readProcess(clients, id); err == nil {
-			return fmt.Errorf("process with ID %s should not exist", id.String())
+		err = retry.RetryContext(clients.Ctx, timeout, func() *retry.RetryError {
+			_, err := readProcess(clients, id)
+			if err == nil {
+				return retry.RetryableError(fmt.Errorf("process with ID %s should not exist", id.String()))
+			}
+			if utils.ResponseWasNotFound(err) {
+				return nil
+			}
+
+			return retry.NonRetryableError(err)
+		})
+		if err != nil {
+			return err
 		}
 	}
 
