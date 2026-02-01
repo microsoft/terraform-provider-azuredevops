9c68193f67dbd421dca0ef013482dcc5706a7fe0
Fredrik Arvidsson <fresa@fresa.se>
2026-02-02 00:39:34 +0100

    Add pages to work item type layout (#1482)
    * feat(page): add page resource for work item type
    
    * test: add page resource to provider expectation list
    
    * fix(page): sections and page type is computed only
    
    * fix(page): locked is not settable
    
    * fix(page): add nil checks
    
    * build: format
    
    * doc(page): simplify example
    
    * refactor(page): use helper for importing id
    
    * test(page): remove redundant attribute assertions
    
    * refactor(page): align with work_item_type_id
    
    * refactor(page): remove redundant attributes
    
    * doc(page): wording
    
    Co-authored-by: magodo <wztdyl@sina.com>
    
    * refactor(page): rename section to sections
    
    * fix(page): harden the usage of getrawconfig to prevent panics
    
    * fix(page): get order correctly from config/state
    
    ---------
    
    Co-authored-by: magodo <wztdyl@sina.com>

--- azuredevops/provider_test.go
+++ azuredevops/provider_test.go
@@ -127,6 +127,7 @@ func TestProvider_HasChildResources(t *testing.T) {
 		"azuredevops_workitemtrackingprocess_control",
 		"azuredevops_workitemtrackingprocess_field",
 		"azuredevops_workitemtrackingprocess_group",
+		"azuredevops_workitemtrackingprocess_page",
 		"azuredevops_workitemtrackingprocess_process",
 		"azuredevops_workitemtrackingprocess_process_permissions",
 		"azuredevops_workitemtrackingprocess_workitemtype",
