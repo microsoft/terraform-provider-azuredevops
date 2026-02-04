438a4d3a957a488214c8bc2afc8affa5b974cfd7
Fredrik Arvidsson <fresa@fresa.se>
2026-02-04 10:09:14 +0100

    test(rule): test all conditions and actions

--- azuredevops/internal/acceptancetests/resource_workitemtrackingprocess_rule_test.go
+++ azuredevops/internal/acceptancetests/resource_workitemtrackingprocess_rule_test.go
@@ -99,6 +99,173 @@ func TestAccWorkitemtrackingprocessRule_MultipleConditionsAndActions(t *testing.
 	})
 }
 
+func TestAccWorkitemtrackingprocessRule_ConditionTypes(t *testing.T) {
+	testCases := []struct {
+		conditionType string
+		field         string
+		value         string
+	}{
+		{"when", "System.State", "New"},
+		{"whenNot", "System.State", "Closed"},
+		{"whenChanged", "System.Title", ""},
+		{"whenNotChanged", "System.Title", ""},
+		{"whenStateChangedTo", "New", ""},
+		{"whenWorkItemIsCreated", "", ""},
+		{"whenValueIsDefined", "System.Title", ""},
+		{"whenValueIsNotDefined", "System.Description", ""},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.conditionType, func(t *testing.T) {
+			workItemTypeName := testutils.GenerateWorkItemTypeName()
+			processName := testutils.GenerateResourceName()
+			tfNode := "azuredevops_workitemtrackingprocess_rule.test"
+
+			resource.ParallelTest(t, resource.TestCase{
+				PreCheck:          func() { testutils.PreCheck(t, nil) },
+				ProviderFactories: testutils.GetProviderFactories(),
+				CheckDestroy:      testutils.CheckProcessDestroyed,
+				Steps: []resource.TestStep{
+					{
+						Config: ruleWithConditionType(workItemTypeName, processName, tc.conditionType, tc.field, tc.value),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestCheckResourceAttrSet(tfNode, "id"),
+						),
+					},
+				},
+			})
+		})
+	}
+}
+
+func TestAccWorkitemtrackingprocessRule_ActionTypes(t *testing.T) {
+	testCases := []struct {
+		actionType  string
+		targetField string
+		value       string
+	}{
+		{"makeRequired", "System.Title", ""},
+		{"makeReadOnly", "System.Title", ""},
+		{"setDefaultValue", "System.Title", "Default Title"},
+		{"setDefaultFromClock", "System.ChangedDate", ""},
+		{"setDefaultFromCurrentUser", "System.AssignedTo", ""},
+		{"setDefaultFromField", "System.Description", "System.Title"},
+		{"copyValue", "System.Title", "Copied Value"},
+		{"copyFromClock", "System.ChangedDate", ""},
+		{"copyFromCurrentUser", "System.AssignedTo", ""},
+		{"copyFromField", "System.Description", "System.Title"},
+		{"setValueToEmpty", "System.Description", ""},
+		{"copyFromServerClock", "System.ChangedDate", ""},
+		{"copyFromServerCurrentUser", "System.AssignedTo", ""},
+		{"hideTargetField", "System.Description", ""},
+		{"disallowValue", "System.State", "Closed"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.actionType, func(t *testing.T) {
+			workItemTypeName := testutils.GenerateWorkItemTypeName()
+			processName := testutils.GenerateResourceName()
+			tfNode := "azuredevops_workitemtrackingprocess_rule.test"
+
+			resource.ParallelTest(t, resource.TestCase{
+				PreCheck:          func() { testutils.PreCheck(t, nil) },
+				ProviderFactories: testutils.GetProviderFactories(),
+				CheckDestroy:      testutils.CheckProcessDestroyed,
+				Steps: []resource.TestStep{
+					{
+						Config: ruleWithActionType(workItemTypeName, processName, tc.actionType, tc.targetField, tc.value),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestCheckResourceAttrSet(tfNode, "id"),
+						),
+					},
+				},
+			})
+		})
+	}
+}
+
+func TestAccWorkitemtrackingprocessRule_ConditionWhenWas(t *testing.T) {
+	workItemTypeName := testutils.GenerateWorkItemTypeName()
+	processName := testutils.GenerateResourceName()
+	tfNode := "azuredevops_workitemtrackingprocess_rule.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testutils.PreCheck(t, nil) },
+		ProviderFactories: testutils.GetProviderFactories(),
+		CheckDestroy:      testutils.CheckProcessDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: ruleWithWhenWas(workItemTypeName, processName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccWorkitemtrackingprocessRule_ConditionWhenStateChangedFromAndTo(t *testing.T) {
+	workItemTypeName := testutils.GenerateWorkItemTypeName()
+	processName := testutils.GenerateResourceName()
+	tfNode := "azuredevops_workitemtrackingprocess_rule.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testutils.PreCheck(t, nil) },
+		ProviderFactories: testutils.GetProviderFactories(),
+		CheckDestroy:      testutils.CheckProcessDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: ruleWithStateChangedFromAndTo(workItemTypeName, processName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccWorkitemtrackingprocessRule_ConditionWhenCurrentUserIsMemberOfGroup(t *testing.T) {
+	workItemTypeName := testutils.GenerateWorkItemTypeName()
+	processName := testutils.GenerateResourceName()
+	groupName := testutils.GenerateResourceName()
+	tfNode := "azuredevops_workitemtrackingprocess_rule.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testutils.PreCheck(t, nil) },
+		ProviderFactories: testutils.GetProviderFactories(),
+		CheckDestroy:      testutils.CheckProcessDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: ruleWithCurrentUserIsMemberOfGroup(workItemTypeName, processName, groupName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccWorkitemtrackingprocessRule_ConditionWhenCurrentUserIsNotMemberOfGroup(t *testing.T) {
+	workItemTypeName := testutils.GenerateWorkItemTypeName()
+	processName := testutils.GenerateResourceName()
+	groupName := testutils.GenerateResourceName()
+	tfNode := "azuredevops_workitemtrackingprocess_rule.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testutils.PreCheck(t, nil) },
+		ProviderFactories: testutils.GetProviderFactories(),
+		CheckDestroy:      testutils.CheckProcessDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: ruleWithCurrentUserIsNotMemberOfGroup(workItemTypeName, processName, groupName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(tfNode, "id"),
+				),
+			},
+		},
+	})
+}
+
 func basicRule(workItemTypeName string, processName string) string {
 	workItemType := basicWorkItemType(workItemTypeName, processName)
 	return fmt.Sprintf(`
@@ -186,6 +353,189 @@ resource "azuredevops_workitemtrackingprocess_rule" "test" {
   }
 }
 `, workItemType)
+}
+
+func ruleWithWhenWas(workItemTypeName, processName string) string {
+	workItemType := basicWorkItemType(workItemTypeName, processName)
+	return fmt.Sprintf(`
+%s
+
+resource "azuredevops_workitemtrackingprocess_rule" "test" {
+  process_id        = azuredevops_workitemtrackingprocess_process.test.id
+  work_item_type_id = azuredevops_workitemtrackingprocess_workitemtype.test.reference_name
+  name              = "Test whenWas Rule"
+
+  condition {
+    condition_type = "whenWas"
+    field          = "System.State"
+    value          = "New"
+  }
+
+  condition {
+    condition_type = "when"
+    field          = "System.State"
+    value          = "Active"
+  }
+
+  action {
+    action_type  = "makeRequired"
+    target_field = "System.Title"
+  }
+}
+`, workItemType)
+}
+
+func ruleWithStateChangedFromAndTo(workItemTypeName, processName string) string {
+	workItemType := basicWorkItemType(workItemTypeName, processName)
+	return fmt.Sprintf(`
+%s
+
+resource "azuredevops_workitemtrackingprocess_rule" "test" {
+  process_id        = azuredevops_workitemtrackingprocess_process.test.id
+  work_item_type_id = azuredevops_workitemtrackingprocess_workitemtype.test.reference_name
+  name              = "Test whenStateChangedFromAndTo Rule"
+
+  condition {
+    condition_type = "whenStateChangedFromAndTo"
+    field          = "System.State"
+    value          = "New;Active"
+  }
+
+  action {
+    action_type  = "makeRequired"
+    target_field = "System.Title"
+  }
+}
+`, workItemType)
+}
+
+func ruleWithCurrentUserIsMemberOfGroup(workItemTypeName, processName, groupName string) string {
+	workItemType := basicWorkItemType(workItemTypeName, processName)
+	return fmt.Sprintf(`
+%s
+
+resource "azuredevops_project" "group_test" {
+  name = "%s-proj"
+}
+
+resource "azuredevops_group" "test" {
+  scope        = azuredevops_project.group_test.id
+  display_name = "%s"
+}
+
+resource "azuredevops_workitemtrackingprocess_rule" "test" {
+  process_id        = azuredevops_workitemtrackingprocess_process.test.id
+  work_item_type_id = azuredevops_workitemtrackingprocess_workitemtype.test.reference_name
+  name              = "Test whenCurrentUserIsMemberOfGroup Rule"
+
+  condition {
+    condition_type = "whenCurrentUserIsMemberOfGroup"
+    value          = azuredevops_group.test.descriptor
+  }
+
+  action {
+    action_type  = "makeRequired"
+    target_field = "System.Title"
+  }
+}
+`, workItemType, groupName, groupName)
+}
+
+func ruleWithCurrentUserIsNotMemberOfGroup(workItemTypeName, processName, groupName string) string {
+	workItemType := basicWorkItemType(workItemTypeName, processName)
+	return fmt.Sprintf(`
+%s
+
+resource "azuredevops_project" "group_test" {
+  name = "%s-proj"
+}
+
+resource "azuredevops_group" "test" {
+  scope        = azuredevops_project.group_test.id
+  display_name = "%s"
+}
+
+resource "azuredevops_workitemtrackingprocess_rule" "test" {
+  process_id        = azuredevops_workitemtrackingprocess_process.test.id
+  work_item_type_id = azuredevops_workitemtrackingprocess_workitemtype.test.reference_name
+  name              = "Test whenCurrentUserIsNotMemberOfGroup Rule"
+
+  condition {
+    condition_type = "whenCurrentUserIsNotMemberOfGroup"
+    value          = azuredevops_group.test.descriptor
+  }
+
+  action {
+    action_type  = "makeRequired"
+    target_field = "System.Title"
+  }
+}
+`, workItemType, groupName, groupName)
+}
+
+func ruleWithConditionType(workItemTypeName, processName, conditionType, field, value string) string {
+	workItemType := basicWorkItemType(workItemTypeName, processName)
+
+	fieldAttr := ""
+	if field != "" {
+		fieldAttr = fmt.Sprintf(`field = "%s"`, field)
+	}
+	valueAttr := ""
+	if value != "" {
+		valueAttr = fmt.Sprintf(`value = "%s"`, value)
+	}
+
+	return fmt.Sprintf(`
+%s
+
+resource "azuredevops_workitemtrackingprocess_rule" "test" {
+  process_id        = azuredevops_workitemtrackingprocess_process.test.id
+  work_item_type_id = azuredevops_workitemtrackingprocess_workitemtype.test.reference_name
+  name              = "Test %s Rule"
+
+  condition {
+    condition_type = "%s"
+    %s
+    %s
+  }
+
+  action {
+    action_type  = "makeRequired"
+    target_field = "System.Title"
+  }
+}
+`, workItemType, conditionType, conditionType, fieldAttr, valueAttr)
+}
+
+func ruleWithActionType(workItemTypeName, processName, actionType, targetField, value string) string {
+	workItemType := basicWorkItemType(workItemTypeName, processName)
+
+	valueAttr := ""
+	if value != "" {
+		valueAttr = fmt.Sprintf(`value = "%s"`, value)
+	}
+
+	return fmt.Sprintf(`
+%s
+
+resource "azuredevops_workitemtrackingprocess_rule" "test" {
+  process_id        = azuredevops_workitemtrackingprocess_process.test.id
+  work_item_type_id = azuredevops_workitemtrackingprocess_workitemtype.test.reference_name
+  name              = "Test %s Rule"
+
+  condition {
+    condition_type = "when"
+    field          = "System.State"
+    value          = "New"
+  }
+
+  action {
+    action_type  = "%s"
+    target_field = "%s"
+    %s
+  }
+}
+`, workItemType, actionType, actionType, targetField, valueAttr)
 }
 
 func ruleImportStateIdFunc(resourceName string) resource.ImportStateIdFunc {
