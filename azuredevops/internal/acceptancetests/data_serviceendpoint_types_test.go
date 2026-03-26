7c8334f0a3ac5d5f8354cad1f982d3b338e9df78
Levente Laszlo Papp <LeventeLaszlo.Papp@kuka.com>
2026-03-26 17:05:46 +0100

    Replace `terraform-plugin-sdk/v2/terraform` with `terraform-plugin-testing/terraform` in test file

--- azuredevops/internal/acceptancetests/data_serviceendpoint_types_test.go
+++ azuredevops/internal/acceptancetests/data_serviceendpoint_types_test.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/acceptancetests/testutils"
 )
